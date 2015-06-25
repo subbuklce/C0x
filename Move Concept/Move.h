75786de178102a2d28f26849d881f285bd9e4b8c
subbuklce <subbu.klce@outlook.com>
2015-06-25 12:05:01 +0530

    modified code for Move concept

--- Move Concept/Move.h
+++ Move Concept/Move.h
@@ -1,7 +1,59 @@
 #ifndef MOVE_H
 #define MOVE_H
 #include<iostream>
+#include<utility>
 using namespace std;
-class first
+class first{
+public:
+int *p=nullptr;
+int size;
+
+first(int size):p(new int[size]),size(size)
+{
+ cout<<"\n Default Constructor\n";
+	for(auto i =0;i < size;i++)
+		this->p[i] = i;
+}
+first(const first& Obj)
+{
+ cout<<"\n copy Constructor\n";
+ if(this == &Obj) {
+	cout<<"\n Same object called for copy constructor\n";
+	}
+ else{
+	cout<<"\n Different Object so copying the contents\n";
+
+	this->p = new int[Obj.size];
+
+	for( auto i=0;i< Obj.size;i++) 
+		this->p[i]=Obj.p[i];
+
+	this->size = Obj.size;
+
+	}
+}
+
+first( first&& Obj) :
+	p((Obj.p)),
+	size((Obj.size))
+{
+	cout<<"\n Move Constructor\n";
+	Obj.p=nullptr;
+	Obj.size =0;
+
+} 
+first add(){
+	first tem(5);
+	return std::move(tem);
+
+}
+first& operator= (const first& obj);
+first&& operator= ( first&& obj);
+~first(){
+cout<<"\n Destructor called \n";
+ delete p;
+}
+
+};
 
 #endif
